02952bc76ea4ff135083f26d4621a2755db50960
gopal-karhale15 <karhalegopal57@gmail.com>
2026-03-19 18:28:59 +0530

    Add IfElse class to check positive or negative numbers

---
+++ 03-conditions/03-conditions/IfElse.java
@@ -0,0 +1,9 @@
+public class IfElse {
+    public static void main(String[] args) {
+        int num = 5;
+        if (num > 0)
+            System.out.println("Positive");
+        else
+            System.out.println("Negative");
+    }
+}
