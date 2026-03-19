b594f03ae0e6c0e84468e4b3cfb0b0c3bbe1a627
gopal-karhale15 <karhalegopal57@gmail.com>
2026-03-19 18:44:40 +0530

    Add ArrayListExample class to demonstrate ArrayList usage

---
+++ 09-collections/09-collections/ArrayListExample.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        System.out.println(list);
+    }
+}
