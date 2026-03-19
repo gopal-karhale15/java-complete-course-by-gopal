fe3a39bd81c13f9e15ed52491604a98bba120eaf
gopal-karhale15 <karhalegopal57@gmail.com>
2026-03-19 18:40:25 +0530

    Add ClassObject with Car class example

---
+++ 07-oops/07-oops/ClassObject.java
@@ -0,0 +1,10 @@
+class Car {
+    String name = "BMW";
+}
+
+public class ClassObject {
+    public static void main(String[] args) {
+        Car c = new Car();
+        System.out.println(c.name);
+    }
+}
